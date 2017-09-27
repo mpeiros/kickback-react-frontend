be58cbd82b1f53b574c71664556ce88a7cf8b20d
Max Peiros <mpeiros@gmail.com>
2017-09-27 11:35:28 -0700

    Add materialize css

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import CreditCardForm from './CreditCardForm';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>DEPOSIT FUNDS</h1>
         <p>$2,100,312 won in the last 24 hours</p>
         <ButtonList />
