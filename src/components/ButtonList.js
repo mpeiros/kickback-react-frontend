fdeea78e3fd120fc7bf46c270711b1536106df74
Max Peiros <mpeiros@gmail.com>
2017-09-27 12:21:17 -0700

    Add conditional styling for selected button

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 
 class ButtonList extends Component {
   state = {
-    funds: ['$5', '$10', '$20', '$40', '$100', '$250', '$1000']
+    funds: ['$5', '$10', '$20', '$40', '$100', '$250', '$1000'],
+    selected: 0
   };
 
+  selectButton({ target }) {
+    this.setState({ selected: this.state.funds.indexOf(target.value) });
+  }
+
   renderButtons() {
-    return this.state.funds.map(amount => {
+    return this.state.funds.map((amount, index) => {
       return (
-        <button className="btn waves-effect waves-light blue-grey darken-4">
+        <button 
+          key={index}
+          className={
+            this.state.selected === index ?
+            "btn waves-effect waves-light light-green" :
+            "btn waves-effect waves-light blue-grey darken-4"
+          }
+          onClick={this.selectButton.bind(this)}
+          value={amount}
+        >
           {amount}
         </button>
       );
