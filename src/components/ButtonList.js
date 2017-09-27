767e8db5ae0eca7be9adf6b8c98eabb3ccefc432
Max Peiros <mpeiros@gmail.com>
2017-09-27 11:44:07 -0700

    Add renderButtons func to ButtonList

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class ButtonList extends Component {
+  state = {
+    funds: ['$5', '$10', '$20', '$40', '$100', '$250', '$1000']
+  };
+
+  renderButtons() {
+    return this.state.funds.map(amount => {
+      return <button className="btn waves-effect waves-light">{amount}</button>
+    });
+  }
+
   render() {
     return (
       <div>
-        ButtonList
+        {this.renderButtons()}
       </div>
     );
   }
