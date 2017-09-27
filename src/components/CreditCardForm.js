6984d359cda37674e9d32677237d9b0f838044af
Max Peiros <mpeiros@gmail.com>
2017-09-27 12:35:44 -0700

    Add credit card form inputs

--- src/components/CreditCardForm.js
+++ src/components/CreditCardForm.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
 
 class CreditCardForm extends Component {
+  state = {
+    cardNumber: '',
+    name: '',
+    date: '',
+    cvc: ''
+  };
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleInputChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
       <div>
-        Form
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input 
+            type="text" 
+            name="cardNumber"
+            value={this.state.cardNumber} 
+            onChange={this.handleInputChange.bind(this)}
+          />
+          <input 
+            type="text" 
+            name="name"
+            value={this.state.name} 
+            onChange={this.handleInputChange.bind(this)}
+          />
+          <input 
+            type="text" 
+            name="date"
+            value={this.state.date} 
+            onChange={this.handleInputChange.bind(this)}
+          />
+         <input 
+            type="text" 
+            name="cvc"
+            value={this.state.cvc} 
+            onChange={this.handleInputChange.bind(this)}
+          />
+          <button className="btn waves-effect waves-light light-green" type="submit">
+            Deposit funds
+          </button>
+        </form>
       </div>
     );
   }
